cb558810042d4f16cdb245445bb712dcab66d6df
bgv26 <bgv26@mail.ru>
2017-03-02 23:35:46 +0300

    First version of custom priority queue

--- src/CustomPriorityQueue.java
+++ src/CustomPriorityQueue.java
@@ -10,9 +10,9 @@ public class CustomPriorityQueue {
         Scanner scanner = new Scanner(System.in);
         int numberOfOperation = scanner.nextInt();
         for (int i = 0; i < numberOfOperation; i++) {
-            String operation = scanner.nextLine();
+            String operation = scanner.next();
             if (operation.contains(INSERT)) {
-                int value = Integer.valueOf(operation.split(" ")[1]);
+                int value = scanner.nextInt();
                 insert(value);
             }
             if (operation.contains(EXTRACT)) {
@@ -22,25 +22,49 @@ public class CustomPriorityQueue {
     }
 
     private static void insert(int value) {
-        siftUp(value);
+        heap.add(value);
+        int lastIndex = heap.size() - 1;
+        siftUp(lastIndex);
     }
 
     private static String extract() {
-        String result = "";
+        String result = String.valueOf(heap.get(0));
+        int lastIndex = heap.size() - 1;
+        heap.set(0, heap.get(lastIndex));
+        heap.remove(lastIndex);
+        siftDown(0);
         return result;
     }
 
-    private static void siftDown(int value) {
+    private static void siftDown(int index) {
+        int lastIndex = heap.size() - 1;
+        int leftIndex = index * 2 <= lastIndex ? index * 2 : lastIndex;
+        int rightIndex = index * 2 + 1 <= lastIndex ? index * 2 + 1 : lastIndex;
+
+        int current = heap.get(index);
+        int left = heap.get(leftIndex);
+        int right = heap.get(rightIndex);
+
+        int next = Math.min(left, right);
+        int nextIndex = next == left ? leftIndex : rightIndex;
+
+        if (current < next) {
+            heap.set(index, next);
+            heap.set(nextIndex, current);
+            siftDown(nextIndex);
+        }
     }
 
-    private static void siftUp(int value) {
-        heap.add(value);
-        int valueIndex = heap.size() - 1;
-        int parent = heap.get(valueIndex / 2);
-        while (value > parent) {
-            heap.add(valueIndex, parent);
-            heap.add(valueIndex / 2, value);
-            valueIndex /= 2;
+    private static void siftUp(int index) {
+        int current = heap.get(index);
+        int parent = heap.get(index / 2);
+        if (current > parent && index > 0) {
+            if (current > parent) {
+                heap.set(index, parent);
+                heap.set(index / 2, current);
+                index /= 2;
+            }
+            siftUp(index);
         }
     }
 }
