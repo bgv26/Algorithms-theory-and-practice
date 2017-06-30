a6bae88018ce2e142c01f6f33b05afc78afa0491
bgv26 <bgv26@mail.ru>
2017-06-30 14:26:05 +0300

    Change merge_sort (in place version)

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -1,36 +1,43 @@
-def merge(array1, array2, count):
-    result = []
-    left, right = 0, 0
+def merge(result, array1, array2):
+    left, right, master, count = 0, 0, 0, 0
     while left < len(array1) and right < len(array2):
         first = array1[left]
         second = array2[right]
         if first <= second:
-            result.append(first)
+            result[master] = first
             left += 1
         else:
             count += len(array1) - left
-            result.append(second)
+            result[master] = second
             right += 1
-    if left == len(array1):
-        result.extend(array2[right:])
-    else:
-        result.extend(array1[left:])
-    return count, result
+        master += 1
+    while left < len(array1):
+        result[master] = array1[left]
+        left += 1
+        master += 1
+    while right < len(array2):
+        result[master] = array2[right]
+        right += 1
+        master += 1
+    return count
 
 
-def merge_sort(array_list, count):
+def merge_sort(array_list):
     if len(array_list) > 1:
         middle = len(array_list) // 2
-        count, left_half = merge_sort(array_list[:middle], count)
-        count, right_half = merge_sort(array_list[middle:], count)
-        count, array_list = merge(left_half, right_half, count)
-    return count, array_list
+        left_half = array_list[:middle]
+        right_half = array_list[middle:]
+        count = merge_sort(left_half)
+        count += merge_sort(right_half)
+        count += merge(array_list, left_half, right_half)
+        return count
+    return 0
 
 
 def main():
     _ = int(input())
     array = list(map(int, input().split()))
-    count, _ = merge_sort(array, 0)
+    count = merge_sort(array)
     print(count)
 
 
